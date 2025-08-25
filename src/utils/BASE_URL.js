badc8c85b280886e6bb9b6aa90a1479c3d8bb31b
MHaris03 <xhkharis2@gmail.com>
2025-08-26 00:12:41 +0500

    solve bug

--- src/utils/BASE_URL.js
+++ src/utils/BASE_URL.js
@@ -1,5 +1,8 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
 
 
 //// Render Paid Deploy link from render by Osama /// 
 // export const BASE_URL = "https://portal-lvi4.onrender.com"
+
+
+export const BASE_URL = "https://jobportal-server-9l1s.onrender.com";
